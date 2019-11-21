2fc929af971b42bf85667730baa2bf69ea660512
ankit.jain <ankit.jain@ankitjains-mbp.clients.asia-pac.shell.com>
2019-11-21 11:56:19 +0530

    new change

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ variable "ankitline" {
 }
 
 module "mikelines" {
-    source = "git@github.com:mhristof/operculiform-karbi.git"
+    source = "git@github.com:mhristof/operculiform-karbi.git?ref=v1.0.0"
 }
 
 output "story" {
-    value = "${module.mikelines.this_is_the_stuff} ${var.ankitline}"  
+    value = "${module.mikelines.this_is_the_stuff_2} ${var.ankitline}"  
 }
